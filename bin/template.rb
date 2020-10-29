6ab1db5211b316b231ca9709189ed925db89d07c
Paul Durbin <hello@pauldurbin.co.uk>
2020-10-29 09:38:28 +0000

    Update bin/migrate

--- bin/template.rb
+++ bin/template.rb
@@ -2,4 +2,5 @@ copy_file "bin/setup", force: true
 copy_file "bin/run_migrations"
 copy_file "bin/update", force: true
 chmod "bin/setup", "+x"
+chmod "bin/run_migrations", "+x"
 chmod "bin/update", "+x"
